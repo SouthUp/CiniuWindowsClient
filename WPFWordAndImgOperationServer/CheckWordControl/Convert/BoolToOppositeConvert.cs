50d123f6213b59d102d61a3768b7f4e5098a424c
FullHouse2222 <senlin.guo@ciniuwang.com>
2018-05-31 19:07:08 +0800

    add

---
+++ WPFWordAndImgOperationServer/CheckWordControl/Convert/BoolToOppositeConvert.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CheckWordControl
+{
+    public class BoolToOppositeConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!string.IsNullOrEmpty(value.ToString()) && value.ToString() == "false")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
