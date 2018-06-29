fbd0d86e50fc04460bab3fe8ccdde1a23b052b81
FullHouse2222 <senlin.guo@ciniuwang.com>
2018-06-29 09:50:33 +0800

    add

---
+++ WPFWordAndImgOperationServer/CheckWordModel/UserStateInfos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckWordModel
+{
+    public class UserStateInfos : ViewModelBase
+    {
+        private bool active = false;
+        public bool Active
+        {
+            get { return active; }
+            set
+            {
+                active = value;
+                RaisePropertyChanged("Active");
+            }
+        }
+
+        private int pointCount = 0;
+        public int PointCount
+        {
+            get { return pointCount; }
+            set
+            {
+                pointCount = value;
+                RaisePropertyChanged("PointCount");
+            }
+        }
+    }
+}
