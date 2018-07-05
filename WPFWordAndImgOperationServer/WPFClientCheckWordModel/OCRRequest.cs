8ab89a60e1b2248ba53e0ef6716fd0472eefd3b3
FullHouse2222 <senlin.guo@ciniuwang.com>
2018-07-05 17:29:15 +0800

    add

---
+++ WPFWordAndImgOperationServer/WPFClientCheckWordModel/OCRRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFClientCheckWordModel
+{
+    public class OCRRequest
+    {
+        public string image { get; set; }
+        public string recognize_granularity = "small";
+        public string vertexes_location = "true";
+    }
+}
