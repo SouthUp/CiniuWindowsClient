6862c97ead8c178e74b3e94c3498fa88efb41aa7
FullHouse2222 <senlin.guo@ciniuwang.com>
2018-07-10 11:30:38 +0800

    add

---
+++ WPFWordAndImgOperationServer/WPFClientCheckWordModel/ConsumeResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFClientCheckWordModel
+{
+    public class ConsumeResponse
+    {
+        public int count { get; set; }
+        public int points { get; set; }
+        public int consumedPoints { get; set; }
+        public int toAccountWords { get; set; }
+        public int accountedWords { get; set; }
+    }
+}
