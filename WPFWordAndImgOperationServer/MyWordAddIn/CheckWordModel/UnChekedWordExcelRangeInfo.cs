f0b5592afe7db81c73145b6dba03b6db27a9b071
FullHouse2222 <senlin.guo@ciniuwang.com>
2018-07-13 15:32:15 +0800

    add

---
+++ WPFWordAndImgOperationServer/MyWordAddIn/CheckWordModel/UnChekedWordExcelRangeInfo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckWordModel
+{
+    public class UnChekedWordExcelRangeInfo : ViewModelBase
+    {
+        public Range Range { get; set; }
+        public string RangeText { get; set; }
+        public List<UnChekedWordInfo> UnChekedWordLists { get; set; }
+    }
+}
