d115c1f9952b23c8d65a84b78d0ffda006a096df
FullHouse2222 <senlin.guo@ciniuwang.com>
2018-08-10 13:20:30 +0800

    add

---
+++ CiNiuWPFClient/CheckWordModel/WordInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CheckWordModel
+{
+    public class WordInfo
+    {
+        public List<Rect> Rects { get; set; }
+        public string UnValidText { get; set; }
+        public string AllText { get; set; }
+    }
+}
