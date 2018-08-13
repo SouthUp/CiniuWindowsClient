8266cb67cd40f6b9626dc4d51b8070a5a7012c89
FullHouse2222 <senlin.guo@ciniuwang.com>
2018-08-13 12:01:46 +0800

    add

---
+++ CiNiuWPFClient/WordAndImgOperationApp/ImgWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using CheckWordEvent;
+using CheckWordModel;
+using CheckWordUtil;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WPFClientCheckWordModel;
+
+namespace WordAndImgOperationApp
+{
+    /// <summary>
+    /// ImgWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class ImgWindow : Window
+    {
+        MyFolderDataViewModel myFolder;
+        public ImgWindow(MyFolderDataViewModel myFolder)
+        {
+            InitializeComponent();
+            this.myFolder = myFolder;
+        }
+        private void TitleGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            element.Source = AppDomain.CurrentDomain.BaseDirectory + @"Resources\Gif\loading.gif";
+        }
+
+        private void MaxBtn_Checked(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Maximized;
+        }
+
+        private void MaxBtn_Unchecked(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Normal;
+        }
+    }
+}
