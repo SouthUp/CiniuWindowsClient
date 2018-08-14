5356add19eeab062d8959fbb8fa3423a616d4cd8
FullHouse2222 <senlin.guo@ciniuwang.com>
2018-08-14 11:39:54 +0800

    add

---
+++ CiNiuWPFClient/WordAndImgOperationApp/SettingControl.xaml.cs
@@ -0,0 +1,53 @@
+﻿using CheckWordEvent;
+using CheckWordUtil;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFClientCheckWordModel;
+
+namespace WordAndImgOperationApp
+{
+    /// <summary>
+    /// SettingControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class SettingControl : UserControl
+    {
+        SettingControlViewModel viewModel = new SettingControlViewModel();
+        public SettingControl()
+        {
+            InitializeComponent();
+            this.DataContext = viewModel;
+        }
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            InitData();
+        }
+        private async void InitData()
+        {
+            Task task = new Task(() => {
+                EventAggregatorRepository.EventAggregator.GetEvent<SettingWindowBusyIndicatorEvent>().Publish(new AppBusyIndicator { IsBusy = true });
+                try
+                {
+                    
+                }
+                catch (Exception ex)
+                { }
+                EventAggregatorRepository.EventAggregator.GetEvent<SettingWindowBusyIndicatorEvent>().Publish(new AppBusyIndicator { IsBusy = false });
+            });
+            task.Start();
+            await task;
+        }
+    }
+}
---
+++ CiNiuWPFClient/WordAndImgOperationApp/SettingControlViewModel.cs
@@ -0,0 +1,98 @@
+﻿using CheckWordModel;
+using CheckWordUtil;
+using Microsoft.Practices.Prism.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WordAndImgOperationApp
+{
+    public class SettingControlViewModel : NotificationObject
+    {
+        private string _wordOfficeVersion = "";
+        public string WordOfficeVersion
+        {
+            get { return _wordOfficeVersion; }
+            set
+            {
+                _wordOfficeVersion = value;
+                RaisePropertyChanged("WordOfficeVersion");
+            }
+        }
+        private string _excelOfficeVersion = "";
+        public string ExcelOfficeVersion
+        {
+            get { return _excelOfficeVersion; }
+            set
+            {
+                _excelOfficeVersion = value;
+                RaisePropertyChanged("ExcelOfficeVersion");
+            }
+        }
+        private bool _hasWordOffice = false;
+        public bool HasWordOffice
+        {
+            get { return _hasWordOffice; }
+            set
+            {
+                _hasWordOffice = value;
+                RaisePropertyChanged("HasWordOffice");
+            }
+        }
+        private bool _hasExcelOffice = false;
+        public bool HasExcelOffice
+        {
+            get { return _hasExcelOffice; }
+            set
+            {
+                _hasExcelOffice = value;
+                RaisePropertyChanged("HasExcelOffice");
+            }
+        }
+        private bool _hasWordOfficeAddIn = false;
+        public bool HasWordOfficeAddIn
+        {
+            get { return _hasWordOfficeAddIn; }
+            set
+            {
+                _hasWordOfficeAddIn = value;
+                RaisePropertyChanged("HasWordOfficeAddIn");
+            }
+        }
+        private bool _hasExcelOfficeAddIn = false;
+        public bool HasExcelOfficeAddIn
+        {
+            get { return _hasExcelOfficeAddIn; }
+            set
+            {
+                _hasExcelOfficeAddIn = value;
+                RaisePropertyChanged("HasExcelOfficeAddIn");
+            }
+        }
+        private bool _isCheckPicInDucument = true;
+        public bool IsCheckPicInDucument
+        {
+            get { return _isCheckPicInDucument; }
+            set
+            {
+                _isCheckPicInDucument = value;
+                RaisePropertyChanged("IsCheckPicInDucument");
+            }
+        }
+        private bool _isUseCustumCi = false;
+        public bool IsUseCustumCi
+        {
+            get { return _isUseCustumCi; }
+            set
+            {
+                _isUseCustumCi = value;
+                RaisePropertyChanged("IsUseCustumCi");
+            }
+        }
+    }
+}
