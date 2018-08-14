94b426a5435bce70d0fae226282564d5b13ea5fc
FullHouse2222 <senlin.guo@ciniuwang.com>
2018-08-14 15:02:09 +0800

    add

---
+++ CiNiuWPFClient/WordAndImgOperationApp/UserInfoControl.xaml.cs
@@ -0,0 +1,79 @@
+﻿using CheckWordEvent;
+using CheckWordUtil;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFClientCheckWordModel;
+
+namespace WordAndImgOperationApp
+{
+    /// <summary>
+    /// UserInfoControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class UserInfoControl : UserControl
+    {
+        UserInfoControlViewModel viewModel = new UserInfoControlViewModel();
+        public UserInfoControl()
+        {
+            InitializeComponent();
+            this.DataContext = viewModel;
+        }
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            InitData();
+        }
+        private async void InitData()
+        {
+            Task task = new Task(() => {
+                EventAggregatorRepository.EventAggregator.GetEvent<SettingWindowBusyIndicatorEvent>().Publish(new AppBusyIndicator { IsBusy = true });
+                try
+                {
+                    viewModel.UserName = UtilSystemVar.UserName;
+                    APIService service = new APIService();
+                    var userStateInfos = service.GetUserStateByToken(UtilSystemVar.UserToken);
+                    if (userStateInfos != null)
+                    {
+                        viewModel.PointCount = userStateInfos.PointCount;
+                    }
+                }
+                catch (Exception ex)
+                { }
+                EventAggregatorRepository.EventAggregator.GetEvent<SettingWindowBusyIndicatorEvent>().Publish(new AppBusyIndicator { IsBusy = false });
+            });
+            task.Start();
+            await task;
+        }
+
+        private void FindPasswordBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ConsumeSeachBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void RechargeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("http://www.ciniuwang.com/pay");
+            }
+            catch (Exception ex)
+            { }
+        }
+    }
+}
---
+++ CiNiuWPFClient/WordAndImgOperationApp/UserInfoControlViewModel.cs
@@ -0,0 +1,38 @@
+﻿using CheckWordModel;
+using CheckWordUtil;
+using Microsoft.Practices.Prism.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WordAndImgOperationApp
+{
+    public class UserInfoControlViewModel : NotificationObject
+    {
+        private string userName = "";
+        public string UserName
+        {
+            get { return userName; }
+            set
+            {
+                userName = value;
+                RaisePropertyChanged("UserName");
+            }
+        }
+        private int pointCount = 0;
+        public int PointCount
+        {
+            get { return pointCount; }
+            set
+            {
+                pointCount = value;
+                RaisePropertyChanged("PointCount");
+            }
+        }
+    }
+}
