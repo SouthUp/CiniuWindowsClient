0ebf3fb11667c4d872ec8ec737e533c5814ebc0e
FullHouse2222 <senlin.guo@ciniuwang.com>
2018-08-14 13:51:18 +0800

    集成

--- CiNiuWPFClient/WordAndImgOperationApp/SettingControl.xaml.cs
+++ CiNiuWPFClient/WordAndImgOperationApp/SettingControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using CheckWordEvent;
 using CheckWordUtil;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -40,7 +41,34 @@ namespace WordAndImgOperationApp
                 EventAggregatorRepository.EventAggregator.GetEvent<SettingWindowBusyIndicatorEvent>().Publish(new AppBusyIndicator { IsBusy = true });
                 try
                 {
-                    
+                    string officeWordVersion = GetOfficeAppVersion("Word");
+                    if (string.IsNullOrEmpty(officeWordVersion))
+                    {
+                        viewModel.HasWordOffice = false;
+                    }
+                    else
+                    {
+                        viewModel.HasWordOffice = true;
+                        viewModel.WordOfficeVersion = officeWordVersion;
+                    }
+                    string officeExcelVersion = GetOfficeAppVersion("Excel");
+                    if (string.IsNullOrEmpty(officeExcelVersion))
+                    {
+                        viewModel.HasExcelOffice = false;
+                    }
+                    else
+                    {
+                        viewModel.HasExcelOffice = true;
+                        viewModel.ExcelOfficeVersion = officeExcelVersion;
+                    }
+                    if (GetHasOfficeAddIn("Word"))
+                    {
+                        viewModel.HasWordOfficeAddIn = true;
+                    }
+                    if (GetHasOfficeAddIn("Excel"))
+                    {
+                        viewModel.HasExcelOfficeAddIn = true;
+                    }
                 }
                 catch (Exception ex)
                 { }
@@ -48,6 +76,72 @@ namespace WordAndImgOperationApp
             });
             task.Start();
             await task;
+        }
+        private string GetOfficeAppVersion(string officeName)
+        {
+            string officeVersion = "";
+            RegistryKey rk;
+            if (Environment.Is64BitOperatingSystem)
+                rk = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            else
+                rk = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
+            RegistryKey akey10 = rk.OpenSubKey(@"SOFTWARE\Microsoft\Office\14.0\" + officeName + @"\InstallRoot\");//查询2010
+            RegistryKey akey13 = rk.OpenSubKey(@"SOFTWARE\Microsoft\Office\15.0\" + officeName + @"\InstallRoot\");//查询2013
+            RegistryKey akey16 = rk.OpenSubKey(@"SOFTWARE\Microsoft\Office\16.0\" + officeName + @"\InstallRoot\");//查询2016
+            if (akey10 != null)
+            {
+                officeVersion = "2010";
+            }
+            if (akey13 != null)
+            {
+                officeVersion = "2013";
+            }
+            if (akey16 != null)
+            {
+                officeVersion = "2016";
+            }
+            return officeVersion;
+        }
+
+        private bool GetHasOfficeAddIn(string addInName)
+        {
+            bool result = false;
+            try
+            {
+                RegistryKey rk;
+                if (Environment.Is64BitOperatingSystem)
+                    rk = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64);
+                else
+                    rk = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry32);
+                RegistryKey akeyAddIns = rk.OpenSubKey(@"Software\Microsoft\Office\" + addInName + @"\Addins\上海冲南智能科技有限公司.词牛" + addInName + "插件");
+                if (akeyAddIns != null)
+                {
+                    result = true;
+                }
+            }
+            catch (Exception ex)
+            { }
+            return result;
+        }
+
+        private void ToggleIsCheckPic_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ToggleIsCheckPic_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ToggleIsUseCustumCi_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ToggleIsUseCustumCi_Unchecked(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
