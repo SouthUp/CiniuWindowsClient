14662bec50abca90787c62e30f97edba6e0ba641
FullHouse2222 <senlin.guo@ciniuwang.com>
2018-08-14 18:09:04 +0800

    add

---
+++ CiNiuWPFClient/WordAndImgOperationApp/SettingFindPsw.xaml.cs
@@ -0,0 +1,249 @@
+﻿using CheckWordEvent;
+using CheckWordUtil;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFClientCheckWordModel;
+
+namespace WordAndImgOperationApp
+{
+    /// <summary>
+    /// SettingFindPsw.xaml 的交互逻辑
+    /// </summary>
+    public partial class SettingFindPsw : UserControl
+    {
+        SettingFindPswViewModel viewModel = new SettingFindPswViewModel();
+        public SettingFindPsw()
+        {
+            InitializeComponent();
+            this.DataContext = viewModel;
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            viewModel.UserName = UtilSystemVar.UserName;
+        }
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (timersTimer != null)
+                {
+                    timersTimer.Stop();
+                }
+            }
+            catch (Exception ex)
+            { }
+        }
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            LoginIn();
+        }
+        /// <summary>
+        /// 登陆
+        /// </summary>
+        private void LoginIn()
+        {
+            viewModel.MessageInfo = "";
+            viewModel.UserName = UserNameTextBox.Text;
+            viewModel.YZMStr = YZMTextBox.Text;
+            if (!FindPswCheckPhoneAndCodePass(viewModel.UserName, viewModel.YZMStr, viewModel.PassWord, viewModel.NewPassWord)) return;
+            EventAggregatorRepository.EventAggregator.GetEvent<SettingWindowBusyIndicatorEvent>().Publish(new AppBusyIndicator() { IsBusy = true });
+            System.Threading.ThreadStart startLogin = delegate ()
+            {
+                APIService service = new APIService();
+                string messageInfo = "";
+                string token = service.FindPsw(viewModel.UserName, viewModel.PassWord, viewModel.YZMStr, out messageInfo);
+                if (!string.IsNullOrEmpty(token))
+                {
+                    UtilSystemVar.UserToken = token;
+                    UtilSystemVar.UserName = viewModel.UserName;
+                    EventAggregatorRepository.EventAggregator.GetEvent<LoginInOrOutEvent>().Publish("LoginIn");
+                    viewModel.FindPswGridVisibility = Visibility.Collapsed;
+                    viewModel.FindPswResultGridVisibility = Visibility.Visible;
+                    Task task = new Task(() =>
+                    {
+                        System.Threading.Thread.Sleep(2000);
+                        EventAggregatorRepository.EventAggregator.GetEvent<LoadSettingWindowGridViewEvent>().Publish("UserInfoControl");
+                    });
+                    task.Start();
+                }
+                else
+                {
+                    viewModel.MessageInfo = messageInfo;
+                }
+                EventAggregatorRepository.EventAggregator.GetEvent<SettingWindowBusyIndicatorEvent>().Publish(new AppBusyIndicator() { IsBusy = false });
+            };
+            System.Threading.Thread t = new System.Threading.Thread(startLogin);
+            t.IsBackground = true;
+            t.Start();
+        }
+        public System.Timers.Timer timersTimer;
+        int countTime = 60;
+        private void SendYZMBtn_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.MessageInfo = "";
+            viewModel.UserName = UserNameTextBox.Text;
+            if (!CheckPhonePass(viewModel.UserName)) return;
+            viewModel.IsSendYZMBtnEnabled = false;
+            System.Threading.ThreadStart startLogin = delegate ()
+            {
+                APIService service = new APIService();
+                string resultSendYZM = service.RegisterSendYZM(viewModel.UserName, "FindPsw");
+                if (resultSendYZM.ToLower() == "ok")
+                {
+                    viewModel.SendYZMBtnContentTime = "60s";
+                    viewModel.SendYZMBtnContent = "重新发送验证码";
+                    viewModel.MessageInfo = "验证码已发送,请注意查收";
+                    timersTimer = new System.Timers.Timer();
+                    timersTimer.Interval = 1000;
+                    timersTimer.Elapsed += TimersTimer_Elapsed;
+                    timersTimer.Start();
+                }
+                else
+                {
+                    viewModel.MessageInfo = resultSendYZM;
+                    viewModel.IsSendYZMBtnEnabled = true;
+                }
+            };
+            System.Threading.Thread t = new System.Threading.Thread(startLogin);
+            t.IsBackground = true;
+            t.Start();
+        }
+        private void TimersTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            countTime--;
+            viewModel.SendYZMBtnContentTime = countTime.ToString() + "s";
+            if (countTime == 0)
+            {
+                timersTimer.Stop();
+                viewModel.IsSendYZMBtnEnabled = true;
+                viewModel.SendYZMBtnContentTime = "";
+                viewModel.SendYZMBtnContent = "重新发送验证码";
+                countTime = 60;
+            }
+        }
+        private bool CheckPhonePass(string userName)
+        {
+            if (String.IsNullOrWhiteSpace(userName))
+            {
+                viewModel.MessageInfo = "请输入手机号码";
+                return false;
+            }
+            else
+            {
+                Regex regex = new Regex(@"^1\d{10}$");// new Regex(@"^1(3|4|5|7|8)\d{9}$");
+                if (!regex.IsMatch(userName))
+                {
+                    viewModel.MessageInfo = "请输入正确的手机号";
+                    return false;
+                }
+            }
+            return true;
+        }
+        private bool FindPswCheckPhoneAndCodePass(string userName, string code,string psw,string newPsw)
+        {
+            if (String.IsNullOrWhiteSpace(userName))
+            {
+                viewModel.MessageInfo = "请输入手机号码";
+                return false;
+            }
+            else
+            {
+                Regex regex = new Regex(@"^1\d{10}$");// new Regex(@"^1(3|4|5|7|8)\d{9}$");
+                if (!regex.IsMatch(userName))
+                {
+                    viewModel.MessageInfo = "请输入正确的手机号";
+                    return false;
+                }
+            }
+            if (String.IsNullOrWhiteSpace(code))
+            {
+                viewModel.MessageInfo = "请输入验证码";
+                return false;
+            }
+            if (String.IsNullOrWhiteSpace(newPsw))
+            {
+                viewModel.MessageInfo = "请输入重置密码";
+                return false;
+            }
+            if (String.IsNullOrWhiteSpace(psw))
+            {
+                viewModel.MessageInfo = "请输入确认密码";
+                return false;
+            }
+            if (psw != newPsw)
+            {
+                viewModel.MessageInfo = "确认密码与重置密码不一致";
+                return false;
+            }
+            return true;
+        }
+
+        private void LoginBtn_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                LoginIn();
+            }
+        }
+
+        private void UserNameTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            UserNameBorder.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ad94d8"));
+        }
+
+        private void UserNameTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            UserNameBorder.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#c3becb"));
+        }
+
+        private void YZMTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            YZMBorder.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ad94d8"));
+        }
+
+        private void YZMTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            YZMBorder.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#c3becb"));
+        }
+
+        private void Password_GotFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordBorder.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ad94d8"));
+        }
+
+        private void Password_LostFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordBorder.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#c3becb"));
+        }
+        private void NewPassword_GotFocus(object sender, RoutedEventArgs e)
+        {
+            NewPasswordBorder.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ad94d8"));
+        }
+
+        private void NewPassword_LostFocus(object sender, RoutedEventArgs e)
+        {
+            NewPasswordBorder.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#c3becb"));
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            EventAggregatorRepository.EventAggregator.GetEvent<LoadSettingWindowGridViewEvent>().Publish("UserInfoControl");
+        }
+    }
+}
---
+++ CiNiuWPFClient/WordAndImgOperationApp/SettingFindPswViewModel.cs
@@ -0,0 +1,145 @@
+﻿using CheckWordModel;
+using CheckWordUtil;
+using Microsoft.Practices.Prism.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WordAndImgOperationApp
+{
+    public class SettingFindPswViewModel : NotificationObject
+    {
+        private string messageInfo = "";
+        public string MessageInfo
+        {
+            get { return messageInfo; }
+            set
+            {
+                messageInfo = value;
+                RaisePropertyChanged("MessageInfo");
+            }
+        }
+        private string _userName;
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                if (_userName != value)
+                {
+                    _userName = value;
+                    RaisePropertyChanged("UserName");
+                }
+            }
+        }
+        private string _passWord;
+        public string PassWord
+        {
+            get { return _passWord; }
+            set
+            {
+                if (_passWord != value)
+                {
+                    _passWord = value;
+                    RaisePropertyChanged("PassWord");
+                }
+            }
+        }
+        private string _newPassWord;
+        public string NewPassWord
+        {
+            get { return _newPassWord; }
+            set
+            {
+                if (_newPassWord != value)
+                {
+                    _newPassWord = value;
+                    RaisePropertyChanged("NewPassWord");
+                }
+            }
+        }
+        private string _yzmStr;
+        public string YZMStr
+        {
+            get { return _yzmStr; }
+            set
+            {
+                if (_yzmStr != value)
+                {
+                    _yzmStr = value;
+                    RaisePropertyChanged("YZMStr");
+                }
+            }
+        }
+        private string _sendYZMBtnContentTime = "";
+        public string SendYZMBtnContentTime
+        {
+            get { return _sendYZMBtnContentTime; }
+            set
+            {
+                if (_sendYZMBtnContentTime != value)
+                {
+                    _sendYZMBtnContentTime = value;
+                    RaisePropertyChanged("SendYZMBtnContentTime");
+                }
+            }
+        }
+        private string _sendYZMBtnContent = "发送验证码";
+        public string SendYZMBtnContent
+        {
+            get { return _sendYZMBtnContent; }
+            set
+            {
+                if (_sendYZMBtnContent != value)
+                {
+                    _sendYZMBtnContent = value;
+                    RaisePropertyChanged("SendYZMBtnContent");
+                }
+            }
+        }
+        private bool _isSendYZMBtnEnabled = true;
+        public bool IsSendYZMBtnEnabled
+        {
+            get { return _isSendYZMBtnEnabled; }
+            set
+            {
+                if (_isSendYZMBtnEnabled != value)
+                {
+                    _isSendYZMBtnEnabled = value;
+                    RaisePropertyChanged("IsSendYZMBtnEnabled");
+                }
+            }
+        }
+        private Visibility _findPswGridVisibility = Visibility.Visible;
+        public Visibility FindPswGridVisibility
+        {
+            get { return _findPswGridVisibility; }
+            set
+            {
+                if (_findPswGridVisibility != value)
+                {
+                    _findPswGridVisibility = value;
+                    RaisePropertyChanged("FindPswGridVisibility");
+                }
+            }
+        }
+        private Visibility _findPswResultGridVisibility = Visibility.Collapsed;
+        public Visibility FindPswResultGridVisibility
+        {
+            get { return _findPswResultGridVisibility; }
+            set
+            {
+                if (_findPswResultGridVisibility != value)
+                {
+                    _findPswResultGridVisibility = value;
+                    RaisePropertyChanged("FindPswResultGridVisibility");
+                }
+            }
+        }
+    }
+}
