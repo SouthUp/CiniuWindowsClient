e69ec2426c50c3db4c6d21028954f608b3ca32de
FullHouse2222 <senlin.guo@ciniuwang.com>
2018-08-15 14:59:33 +0800

    集成

---
+++ CiNiuWPFClient/WPFClientCheckWordModel/WordDiscriptionsModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFClientCheckWordModel
+{
+    public class WordDiscriptionsModel
+    {
+        public List<CommonNameModel> Discriptions { get; set; }
+    }
+}
