dfbd0407f8d563f26dc4fa58dd83df94309b6515
FullHouse2222 <senlin.guo@ciniuwang.com>
2018-08-17 18:26:18 +0800

    add

---
+++ CiNiuWPFClient/CheckWordModel/CustumCiInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CheckWordModel
+{
+    public class CustumCiInfo : ViewModelBase
+    {
+        private string id = "";
+        public string ID
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                RaisePropertyChanged("ID");
+            }
+        }
+        private string name = "";
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                RaisePropertyChanged("Name");
+            }
+        }
+        private string discriptionInfo = "";
+        public string DiscriptionInfo
+        {
+            get { return discriptionInfo; }
+            set
+            {
+                discriptionInfo = value;
+                RaisePropertyChanged("DiscriptionInfo");
+            }
+        }
+    }
+}
