d56a666cfeb39d65139a15b2e258ce6206f677d3
FullHouse2222 <senlin.guo@ciniuwang.com>
2018-08-20 16:04:30 +0800

    add

---
+++ CiNiuWPFClient/WPFClientCheckWordModel/CustumWordRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFClientCheckWordModel
+{
+    public class CustumWordRequest
+    {
+        public string wordId { get; set; }
+        public string name { get; set; }
+        public bool sensitive { get; set; }
+        public bool official { get; set; }
+        public string comment { get; set; }
+    }
+}
