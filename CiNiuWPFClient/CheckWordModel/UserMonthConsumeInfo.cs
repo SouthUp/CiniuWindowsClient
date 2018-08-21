14c534c6531bb67dcb6b4882f8407829483b871d
FullHouse2222 <senlin.guo@ciniuwang.com>
2018-08-21 17:06:08 +0800

    add

---
+++ CiNiuWPFClient/CheckWordModel/UserMonthConsumeInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CheckWordModel
+{
+    public class UserMonthConsumeInfo : ViewModelBase
+    {
+        private int totalCount = 0;
+        public int TotalConsumeCount
+        {
+            get { return totalCount; }
+            set
+            {
+                totalCount = value;
+                RaisePropertyChanged("TotalCount");
+            }
+        }
+        private int wordConsumeCount = 0;
+        public int WordConsumeCount
+        {
+            get { return wordConsumeCount; }
+            set
+            {
+                wordConsumeCount = value;
+                RaisePropertyChanged("WordConsumeCount");
+            }
+        }
+        private int picConsumeCount = 0;
+        public int PicConsumeCount
+        {
+            get { return picConsumeCount; }
+            set
+            {
+                picConsumeCount = value;
+                RaisePropertyChanged("PicConsumeCount");
+            }
+        }
+    }
+}
