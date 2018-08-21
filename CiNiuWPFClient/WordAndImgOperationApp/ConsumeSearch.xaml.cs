3cd5640a670894a851213f148f6f1dbe8eb5f84b
FullHouse2222 <senlin.guo@ciniuwang.com>
2018-08-21 14:24:08 +0800

    add

---
+++ CiNiuWPFClient/WordAndImgOperationApp/ConsumeSearch.xaml.cs
@@ -0,0 +1,45 @@
+﻿using CheckWordEvent;
+using CheckWordUtil;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFClientCheckWordModel;
+
+namespace WordAndImgOperationApp
+{
+    /// <summary>
+    /// ConsumeSearch.xaml 的交互逻辑
+    /// </summary>
+    public partial class ConsumeSearch : UserControl
+    {
+        ConsumeSearchViewModel viewModel = new ConsumeSearchViewModel();
+        public ConsumeSearch()
+        {
+            InitializeComponent();
+            this.DataContext = viewModel;
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void ReturnBtn_Click(object sender, RoutedEventArgs e)
+        {
+            EventAggregatorRepository.EventAggregator.GetEvent<LoadSettingWindowGridViewEvent>().Publish("UserInfoControl");
+        }
+    }
+}
