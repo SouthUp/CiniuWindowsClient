09e749615bb67ff3ddf1e9bbfd707558f228bd81
FullHouse2222 <senlin.guo@ciniuwang.com>
2018-09-03 16:16:24 +0800

    add

---
+++ CiNiuWPFClient/MyWordAddIn/CheckWordModel/CategorySelectInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CheckWordModel
+{
+    public class CategorySelectInfo : ViewModelBase
+    {
+        private bool checkedState = true;
+        public bool CheckedState
+        {
+            get { return checkedState; }
+            set
+            {
+                checkedState = value;
+                RaisePropertyChanged("CheckedState");
+            }
+        }
+        private string name = "";
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                RaisePropertyChanged("Name");
+            }
+        }
+        private string code = "";
+        public string Code
+        {
+            get { return code; }
+            set
+            {
+                code = value;
+                RaisePropertyChanged("Code");
+            }
+        }
+    }
+}
---
+++ CiNiuWPFClient/MyWordAddIn/CheckWordModel/MySettingInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CheckWordModel
+{
+    public class MySettingInfo : ViewModelBase
+    {
+        private bool isCheckPicInDucument = true;
+        public bool IsCheckPicInDucument
+        {
+            get { return isCheckPicInDucument; }
+            set
+            {
+                isCheckPicInDucument = value;
+                RaisePropertyChanged("IsCheckPicInDucument");
+            }
+        }
+        private bool isUseCustumCi = false;
+        public bool IsUseCustumCi
+        {
+            get { return isUseCustumCi; }
+            set
+            {
+                isUseCustumCi = value;
+                RaisePropertyChanged("IsUseCustumCi");
+            }
+        }
+        private List<CategorySelectInfo> categoryInfos = new List<CategorySelectInfo>();
+        public List<CategorySelectInfo> CategoryInfos
+        {
+            get { return categoryInfos; }
+            set
+            {
+                categoryInfos = value;
+                RaisePropertyChanged("CategoryInfos");
+            }
+        }
+    }
+}
